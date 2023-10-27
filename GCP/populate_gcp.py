49c2f4dc13c457d5c77b039c96edc85d0b306942
helzheng123 <helen.zheng@stonybrook.edu>
2023-10-27 03:38:58 +0000

    upload

---
+++ GCP/populate_gcp.py
@@ -0,0 +1,56 @@
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+from faker import Faker
+from gcp import Patient, LaboratoryTest
+import os
+import random 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+## Database credentials 
+DB_HOST = os.getenv("DB_HOST")
+DB_DATABASE = os.getenv("DB_DATABASE")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_CHARSET = os.getenv("DB_CHARSET", "utf8mb4")
+
+# Connection string and creating the engine 
+connect_args={'ssl':{'fake_flag_to_enable_tls': True}}
+connection_string = (f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}'
+                    f"?charset={DB_CHARSET}")
+
+engine = create_engine(
+        connection_string,
+        connect_args=connect_args)
+
+# Creating a session to populate the data
+Session = sessionmaker(bind=engine)
+session = Session()
+
+fake = Faker()
+
+for _ in range(10):
+    patient = Patient(
+        first_name=fake.first_name(),
+        last_name=fake.last_name(),
+        date_of_birth=fake.date_of_birth(minimum_age=10, maximum_age=100),
+        contact_number = fake.phone_number()
+    )
+    session.add(patient)
+
+for _ in range(20):
+    labtest = LaboratoryTest(
+        test_name=fake.first_name(),
+        test_date=fake.date_between(start_date="-5y", end_date="today"),
+        test_result=fake.random_element(elements=('Abnormal', 'Normal')),
+        labtest_id=session.query(Patient).order_by(func.rand()).first().id
+    )
+    session.add(labtest)
+
+# Commit the changes to the database
+session.commit()
+
+# Close the session
+session.close()
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cloud_db_mgmt_pooling_migrations
 Gain practical experience in managing a cloud-based MySQL database with a focus on implementing connection pooling and performing database migrations. You will work with both Azure and Google Cloud Platform (GCP) for this assignment.
+
+## 1. Connection Pooling Setup:
+ - Azure:
+    - Go to Azure Database for MySQL servers --> Click Create --> Choose Flexible Server --> Choose a Resource Group/Create new one --> Enter a server name and change compute + storage to Standard_B1s --> Enter server admin login name, password, confirm password --> Click Next: Networking --> Click + Add 0.0.0.0 - 255.255.255.255 and + Add current client IP address --> Hit create
+    - Once created, go to Server Parameters and change the max_connections to 20 and connect_timeout to 3, change require_secure_transport to OFF
+ - GCP:
+    - Go to SQL--> Create Instance --> Choose MySQL --> Enter an Instance ID and a Password --> Choose Enterprise and change the preset to Sandbox --> Click Show Configuration Options --> Go to Machine Configuration and choose Shared Core and 1vCPU, 0.614 GB --> Go to Connections and Add a Network --> Name it Allow-All and set it as 0.0.0.0/0
+
+## 2a. Database Schema and Data:
+ - Azure and GCP: I created two tables (Patients and Laboratory Test)
